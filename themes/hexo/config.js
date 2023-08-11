17fd723bc20f8722d80cbf65c8bababbf2ba8d2a
zslo <53238398+zslo@users.noreply.github.com>
2023-08-11 17:01:52 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -19,9 +19,9 @@ const CONFIG = {
   POST_LIST_COVER: true, // 列表显示文章封面
   POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
 
-  POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
+  POST_LIST_COVER_DEFAULT: false, // 封面为空时用站点背景做默认封面
   POST_LIST_SUMMARY: true, // 文章摘要
-  POST_LIST_PREVIEW: false, // 读取文章预览
+  POST_LIST_PREVIEW: true, // 读取文章预览
   POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
   ARTICLE_ADJACENT: false, // 显示上一篇下一篇文章推荐
@@ -33,6 +33,6 @@ const CONFIG = {
   WIDGET_TO_TOP: true,
   WIDGET_TO_COMMENT: true, // 跳到评论区
   WIDGET_DARK_MODE: true, // 夜间模式
-  WIDGET_TOC: true // 移动端悬浮目录
+  WIDGET_TOC: false // 移动端悬浮目录
 }
 export default CONFIG

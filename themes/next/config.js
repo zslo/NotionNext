b98e85acec7703c5522f1ccc4f373eb96e483bf3
zslo <53238398+zslo@users.noreply.github.com>
2023-08-11 15:54:48 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
 
-  HOME_BANNER: true, // 首页是否显示大图及标语 [true,false]
+  HOME_BANNER: false, // 首页是否显示大图及标语 [true,false]
   HOME_BANNER_Strings: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
   NAV_TYPE: 'autoCollapse', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
-  POST_LIST_COVER: true, // 文章列表显示封面图
+  POST_LIST_COVER: false, // 文章列表显示封面图
   POST_LIST_PREVIEW: true, // 显示文章预览
   POST_LIST_SUMMARY: true, // 显示用户自定义摘要，有预览时优先只展示预览
 
@@ -30,9 +30,9 @@ const CONFIG = {
   WIDGET_DARK_MODE: false, // 显示日间/夜间模式切换
   WIDGET_TOC: true, // 移动端显示悬浮目录
 
-  ARTICLE_RELATE_POSTS: true, // 相关文章推荐
+  ARTICLE_RELATE_POSTS: false, // 相关文章推荐
   ARTICLE_COPYRIGHT: true, // 文章版权声明
-  ARTICLE_INFO: false, // 显示文章信息
+  ARTICLE_INFO: true, // 显示文章信息
 
 }
 

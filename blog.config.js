2c96a99ce736d548b730894cbc69b3165641dd24
zslo <53238398+zslo@users.noreply.github.com>
2023-08-12 11:12:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,12 +13,12 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
  
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS ||'石祖静谧燃烧<br>意识躁郁排泄<br>生命黑夜逃逸<br>堕入原始洞穴',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS ||'石祖静谧燃烧<br>意识躁郁排泄<br>生命黑夜逃逸<br> 堕入原始洞穴',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '香蕉鱼', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '在洞穴里<br>斜目而视', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO ||'在洞穴里 <br> 斜目而视',// 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://zslo.club', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 

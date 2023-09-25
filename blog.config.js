356b193809e31ea1d8dd6c6bbe66f33e3dc32c1b
zslo <53238398+zslo@users.noreply.github.com>
2023-09-25 14:03:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '香蕉鱼', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO ||'',// 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://zslo.club', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://zslo.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
  
